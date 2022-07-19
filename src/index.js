b5443811aec03a2ed4ac8aa276bdbde684f6c263
Mauricio <tryptic-st@outlook.com>
2022-07-19 13:53:16 -0500

    Add all the Js files, components and copy

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import TodoContainer from "./functionBased/components/TodoContainer"
+import { BrowserRouter as Router } from "react-router-dom"
+
+//stylesheet
+import "./functionBased/App.css"
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <TodoContainer />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
+)
